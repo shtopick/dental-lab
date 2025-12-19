917514360f719ced370bbf5fa4c4af57500e717a
Artur Nefedov <nefartur@mail.ru>
2025-12-19 19:49:04 +0300

    need to fix

---
+++ chat/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'chat'
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('doctors/', views.DoctorListView.as_view(), name='doctor_list'),
+]
--- dental_lab/settings.py
+++ dental_lab/settings.py
@@ -59,10 +59,13 @@ ROOT_URLCONF = 'dental_lab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [
+            BASE_DIR / 'templates',  # ← Глобальные шаблоны в корне
+        ],
+        'APP_DIRS': True,  # ← Ищет шаблоны в папках templates внутри приложений
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -120,3 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # ← Папка static в корне проекта
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
